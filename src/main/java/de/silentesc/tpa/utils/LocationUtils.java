2c93ea8a723e76ca4a647f56293d99e18c258137
silentesc <dominic.woehrle@gmail.com>
2023-07-05 16:30:23 +0200

    Improve teleport sound

--- src/main/java/de/silentesc/tpa/utils/LocationUtils.java
+++ src/main/java/de/silentesc/tpa/utils/LocationUtils.java
@@ -2,7 +2,11 @@ package de.silentesc.tpa.utils;
 
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LocationUtils {
     /*
@@ -12,7 +16,13 @@ public class LocationUtils {
 
     // Teleport player to location and play sound for all player in a specific radius
     public void teleportPlayer(Player player, Location location) {
+        // Teleport player
         player.teleport(location);
-        player.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
+        // Play sound for all players around
+        List<Entity> nearByPlayers = player.getNearbyEntities(20d, 20d, 20d).stream().filter(e -> (e instanceof Player)).collect(Collectors.toList());
+        for (Entity entity : nearByPlayers) {
+            Player p = (Player) entity;
+            p.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
+        }
     }
 }
